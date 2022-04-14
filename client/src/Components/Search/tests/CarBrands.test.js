b2742b33b0431c9e977381f1642d510f1cde9f48
Dhruv Parmar <56183031+P-Dhruv20@users.noreply.github.com>
2022-04-13 21:59:42 -0700

    Added unit tests for rendering

---
+++ client/src/Components/Search/tests/CarBrands.test.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import ReactDOM from 'react-dom'; 
+import CarBrands from '../CarBrands';
+
+it("renders without crashing", ()=>{
+    const div = document.createElement("div");
+    ReactDOM.render(<div />, div);
+    ReactDOM.unmountComponentAtNode(div);
+});
