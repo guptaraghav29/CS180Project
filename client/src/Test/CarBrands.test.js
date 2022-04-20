dfc6477f7cb9e847838befb54ac948791a9775cf
Yug Patel <57920851+yugpatell@users.noreply.github.com>
2022-04-20 16:35:15 -0700

    Update CarBrands.test.js

--- client/src/Test/CarBrands.test.js
+++ client/src/Test/CarBrands.test.js
@@ -1,13 +1,34 @@
-import { Experimental_CssVarsProvider } from '@mui/material';
-import {render, screen, cleanup } from '@testing-library/react'
-import CarBrands from '../Components/Search/CarBrands';
+import * as React from "react";
+import * as ReactDOM from "react-dom";
+import CarBrands from "../Components/Search/CarBrands";
+import App from "../App";
+import {
+  fireEvent,
+  getQueriesForElement,
+  render,
+} from "@testing-library/react";
 
-afterEach(() => {
-   cleanup();
+it("renders the content without any crashes", () => {
+  const root = document.createElement("root");
+  ReactDOM.render(<CarBrands />, root);
+  const { getByLabelText } = getQueriesForElement(root);
+
+  expect(getByLabelText("Brand")).not.toBeNull();
 });
 
-test("renders without crashing", ()=>{
-   render(<CarBrands/>);
-   const CarBrandsComponent = screen.getByTestId('car-brands');
-   expect(CarBrandsComponent).toBeInTheDocument();
+test("change input selection", () => {
+  const { getByLabelText } = render(<App />);
+  const brandInput = getByLabelText("Brand");
+
+  // Default value
+  expect(brandInput.value).toEqual("");
+
+  fireEvent.change(brandInput, { target: { value: "honda" } });
+  expect(brandInput.value).toEqual("honda");
+
+  fireEvent.change(brandInput, { target: { value: "mercedes" } });
+  expect(brandInput.value).toEqual("mercedes");
+
+  fireEvent.change(brandInput, { target: { value: "" } });
+  expect(brandInput.value).toEqual("");
 });
