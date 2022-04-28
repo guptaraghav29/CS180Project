41933c24cc8a2bb4cdfb27e3a5f43cff1159d927
Brij Shah <bshah016@ucr.edu>
2022-04-27 19:51:52 -0700

    Add navbar styling and functionality + average price functionality

--- client/src/Components/NavBar/NavBar.js
+++ client/src/Components/NavBar/NavBar.js
@@ -1,28 +1,61 @@
+import { red } from "@mui/material/colors";
 import React from "react";
+
+function changeStyleAnalytics() {
+  //document.getElementById("analytics").style.cssText = 'background: #3B82F6; color: white;';
+  document.getElementById("analytics").style.cssText = 'border-bottom-color: #3B82F6;';
+  //document.getElementById("bar").style.cssText = 'background-image: -webkit-linear-gradient(bottom, #3B82F6 -3px, transparent -3px, transparent 3px)';
+  document.getElementById("search").style.cssText = 'background: white; color: #3B82F6';
+  document.getElementById("adv").style.cssText = 'background: white; color: #3B82F6';
+}
+function changeStyleSearch() {
+  //document.getElementById("search").style.cssText = 'background: #3B82F6; color: white';
+  document.getElementById("search").style.cssText = 'border-bottom-color: #3B82F6;';
+  //document.getElementById("bar").style.cssText = 'background-image: -webkit-linear-gradient(bottom, #3B82F6 3px, transparent 3px, transparent 3px)';
+  document.getElementById("analytics").style.cssText = 'background: white; color: #3B82F6';
+  document.getElementById("adv").style.cssText = 'background: white; color: #3B82F6';
+}
+function changeStyleAdvSearch() {
+  document.getElementById("search").style.cssText = 'background: white; color: #3B82F6';
+  document.getElementById("analytics").style.cssText = 'background: white; color: #3B82F6';
+  document.getElementById("adv").style.cssText = 'border-bottom-color: #3B82F6;';
+  //document.getElementById("bar").style.cssText = 'border-bottom-color: ';
+  //document.getElementById("bar").style.cssText = 'background-image: -webkit-linear-gradient(bottom, #3B82F6 3px, transparent 3px, transparent 3px)';
+  //document.getElementById("adv").style.cssText = 'background: #3B82F6; color: white';
+  //linear-gradient(left, #3B82F6, #3B82F6; 70%, transparent 70%, transparent 100%); background-image: -webkit-linear-gradient(left, #3B82F6, #3B82F6 70%, transparent 70%, transparent 100%)
+}
 
 const NavBar = () => {
   return (
-    <ul className="flex border-b">
+    <ul className="flex border-color-blue-500" id = "bar">
       <li className="-mb-px mr-1">
         <a
-          className="bg-white inline-block border-l border-t border-r rounded-t py-2 px-4 text-blue-700 font-semibold"
-          href="#models"
+          className="border-b-4 border-sky-blue-500 inline-block rounded-t py-2 px-4 text-blue-500 font-semibold hover:text-blue-800 font-semibold"
+          id = "search"
+          href="#search"
+          onClick={ () => { changeStyleSearch(); }}
         >
           Search
         </a>
       </li>
       <li className="mr-1">
         <a
-          className="bg-white inline-block py-2 px-4 text-blue-500 hover:text-blue-800 font-semibold"
-          href="#prob"
+          className="border-b-4 border-sky-blue-500 inline-block rounded-t py-2 px-4 text-blue-500 font-semibold hover:text-blue-800 font-semibold" 
+          id = "analytics"
+          href="#analysis" 
+          onClick={ () => { 
+            changeStyleAnalytics(); 
+          }}
         >
-          Probability
+          Analytics
         </a>
       </li>
       <li className="mr-1">
         <a
-          className="bg-white inline-block py-2 px-4 text-blue-500 hover:text-blue-800 font-semibold"
+          className="border-b-4 border-sky-blue-500 inline-block rounded-t py-2 px-4 text-blue-500 font-semibold hover:text-blue-800 font-semibold"
+          id = "adv"
           href="#adv"
+          onClick={ () => { changeStyleAdvSearch(); } }
         >
           Advanced Search
         </a>
@@ -31,4 +64,6 @@ const NavBar = () => {
   );
 };
 
+
 export default NavBar;
+
