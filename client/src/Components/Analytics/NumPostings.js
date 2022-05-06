3d18cf5a2ae37d9f88845a7c033685ea811bd019
Raghav Gupta <2001guptar@gmail.com>
2022-05-06 09:54:41 -0700

    tweaked numpostings component

--- client/src/Components/Analytics/NumPostings.js
+++ client/src/Components/Analytics/NumPostings.js
@@ -1,25 +1,26 @@
 import React, { useState, useEffect } from "react";
 
 const NumPostings = ({ carTableData, currentSelection }) => {
-  const [averagePrice, setAveragePrice] = useState(0);
+  const [numberPostings, setNumberOfPostings] = useState(0);
 
   useEffect(() => {
     let selectedChoices = currentSelection;
     let price = 0;
     carTableData.forEach((car) => {
       if (selectedChoices.includes(car.id)) {
-        price += parseInt(car.price);
+        price += parseInt(car.state);
         selectedChoices = selectedChoices.filter((id) => id !== car.id);
       }
     });
-    setAveragePrice(
+
+    setNumberOfPostings(
       currentSelection.length === 0
         ? 0
         : Math.floor(price / currentSelection.length)
     );
   }, [carTableData, currentSelection]);
 
-  return <div>Average Price by Location: {averagePrice}</div>;
+  return <div>Number of Postings by Location: {NumPostings}</div>;
 };
 
 export default NumPostings;
